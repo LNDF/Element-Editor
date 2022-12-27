0e471f085906704ab199379f218d35cd313dd064
LNDF <landergallastegi@gmail.com>
2022-12-27 19:36:53 +0100

    Window implementation stub and entrypoint

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,30 @@
 #include <core/application.h>
 #include <core/log.h>
+#include <event/event.h>
+#include <event/core_events.h>
+
+using namespace element;
+
+bool update_callback(const events::update& e) {
+    static int counter = 5;
+    ELM_DEBUG("Update counter: {0}", counter);
+    if (counter-- == 0) {
+        event_manager::post_event<events::close>({});
+    }
+    return true;
+}
 
 int main() {
-    element::application::setup_engine({});
-    ELM_INFO("TEST");
-    element::application::cleanup_engine();
+    application_settings settings;
+    settings.app_name = "Element editor";
+    settings.app_version = ELM_EDITOR_VERSION;
+    settings.window_height = 0;
+    settings.window_width = 0;
+    settings.window_x = 0;
+    settings.window_y = 0;
+    application::setup_engine(settings);
+    event_manager::register_listener<events::update>(update_callback);
+    application::start();
+    application::cleanup_engine();
     return 0;
 }
--- src/window_stub_temp.cpp
+++ src/window_stub_temp.cpp
@@ -5,75 +5,75 @@
 
 static std::string empty = "";
 
-using namespace element::window;
+using namespace element;
 
-void init_backend() {
+void window::init_backend() {
     ELM_WARN("STUBBED");
 }
 
-void cleanup_backend() {
+void window::cleanup_backend() {
     ELM_WARN("STUBBED");
 }
 
-void create_window(const std::string& title, int width, int height, int x, int y) {
+void window::create_window(const std::string& title, int width, int height, int x, int y) {
     ELM_WARN("STUBBED");
 }
 
-void close_window() {
+void window::close_window() {
     ELM_WARN("STUBBED");
 }
 
-void set_title(const std::string& title) {
+void window::set_title(const std::string& title) {
     ELM_WARN("STUBBED");
 }
 
-const std::string& get_title() {
+const std::string& window::get_title() {
     ELM_WARN("STUBBED");
     return empty;
 }
 
-int get_width() {
+int window::get_width() {
     ELM_WARN("STUBBED");
     return 0;
 }
 
-int get_height() {
+int window::get_height() {
     ELM_WARN("STUBBED");
     return 0;
 }
 
-int get_x_position() {
+int window::get_x_position() {
     ELM_WARN("STUBBED");
     return 0;
 }
 
-int get_y_position() {
+int window::get_y_position() {
     ELM_WARN("STUBBED");
     return 0;
 }
 
-void set_size(int width, int height) {
+void window::set_size(int width, int height) {
     ELM_WARN("STUBBED");
 }
 
-void move_window(int x, int y) {
+void window::move_window(int x, int y) {
     ELM_WARN("STUBBED");
 }
 
-void set_pointer_lock(bool lock) {
+void window::set_pointer_lock(bool lock) {
     ELM_WARN("STUBBED");
 }
 
-bool is_pointer_locked() {
+bool window::is_pointer_locked() {
     ELM_WARN("STUBBED");
     return false;
 }
 
-void set_vsync(bool lock) {
+void window::set_vsync(bool lock) {
     ELM_WARN("STUBBED");
 }
 
-bool is_vsync_enabled() {
+bool window::is_vsync_enabled() {
     ELM_WARN("STUBBED");
     return false;
 }
