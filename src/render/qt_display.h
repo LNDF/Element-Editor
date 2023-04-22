1187932e30588fb710c0914056e8559dd951f8a3
LNDF <landergallastegi@gmail.com>
2023-04-22 15:10:22 +0200

    Remove OpenGL support

--- src/render/qt_display.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <QWidget>
-
-namespace element {
-    void qt_display_set_game_widget(QWidget* widget);
-    QWidget* qt_display_get_game_widget();
-} // namespace element
--- src/ui/element_editor.cpp
+++ src/ui/element_editor.cpp
@@ -2,25 +2,13 @@
 
 #include <core/log.h>
 #include <core/engine.h>
-#include <render/display.h>
-#include <render/qt_display.h>
-#include <render/opengl_qt/qt_element_gl_widget.h>
 
 using namespace element::ui;
 
 element_editor::element_editor() {
     setupUi(this);
-    QWidget* game_widget = nullptr; //Ownership transfered
-    if (engine::settings.renderer == opengl) {
-        ELM_DEBUG("Creating Qt widgets for OpenGL...");
-        game_widget = new qt_element_gl_widget();
-    } else {
-        ELM_ERROR("No rendering API specified. Skipping render widget creation.");
-    }
-    render_canvas->layout()->addWidget(game_widget);
-    qt_display_set_game_widget(game_widget);
-    // create display by default
-    create_display();
+    //render_canvas->layout()->addWidget(game_widget);
+    
 }
 
 element_editor::~element_editor() {
