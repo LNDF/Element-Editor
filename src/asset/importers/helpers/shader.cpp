447f976dd63beb4cf139d75ccbe06f73eabe0dd3
Lander Gallastegi <landergallastegi@gmail.com>
2023-11-03 01:01:41 +0100

    Add shader includes dependencies

--- src/asset/importers/helpers/shader.cpp
+++ src/asset/importers/helpers/shader.cpp
@@ -2,8 +2,11 @@
 
 #include <core/log.h>
 #include <asset/importer.h>
+#include <asset/dependencies.h>
 #include <core/fs_editor.h>
 #include <editor/project.h>
+#include <utils/packed_set.h>
+#include <filesystem>
 
 using namespace element;
 
@@ -15,5 +18,13 @@ void element::importers::helpers::shader_compile(const uuid& id, shader::shader_
     if (result.spv.size() > 0) {
         auto output = fs::get_resource_ostream(id);
         output->write(reinterpret_cast<const char*>(result.spv.data()), sizeof(std::uint32_t) * result.spv.size());
+    } else {
+        fs::delete_resource_data(id);
     }
+    packed_set<uuid> dependencies;
+    dependencies.reserve(result.local_includes.size());
+    for (const std::filesystem::path& inc_path : result.local_includes) {
+        dependencies.insert(fs::get_uuid_from_resource_path(asset_importer::get_fs_path_from_system(inc_path)));
+    }
+    asset_importer::set_dependencies(id, std::move(dependencies));
 }
