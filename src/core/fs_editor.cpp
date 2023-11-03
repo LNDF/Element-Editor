1008866ce6157d9a5a561620ae2eb32b32d9564b
Lander Gallastegi <landergallastegi@gmail.com>
2023-11-03 01:00:19 +0100

    Fix fs editor formatting

--- src/core/fs_editor.cpp
+++ src/core/fs_editor.cpp
@@ -16,106 +16,105 @@
 static std::unordered_map<element::uuid, element::fs_resource_info> fs_map;
 static std::unordered_map<std::string, element::uuid> fs_uuid_map;
 
-namespace element {
-    std::unique_ptr<std::istream> fs::get_resource(const uuid& id) {
-        std::filesystem::path path = project::project_fs_path / id.str();
-        path.make_preferred();
-        return std::unique_ptr<std::istream>(new std::ifstream(path, std::ios::binary));
-    }
+using namespace element;
 
-    std::unique_ptr<std::ostream> fs::get_resource_ostream(const uuid& id) {
-        std::filesystem::path path = project::project_fs_path / id.str();
-        path.make_preferred();
-        return std::unique_ptr<std::ostream>(new std::ofstream(path, std::ios::binary));
-    }
+std::unique_ptr<std::istream> fs::get_resource(const uuid& id) {
+    std::filesystem::path path = project::project_fs_path / id.str();
+    path.make_preferred();
+    return std::unique_ptr<std::istream>(new std::ifstream(path, std::ios::binary));
+}
 
-    const fs_resource_info& fs::get_resource_info(const uuid& id) {
-        return fs_map.at(id);
-    }
+std::unique_ptr<std::ostream> fs::get_resource_ostream(const uuid& id) {
+    std::filesystem::path path = project::project_fs_path / id.str();
+    path.make_preferred();
+    return std::unique_ptr<std::ostream>(new std::ofstream(path, std::ios::binary));
+}
 
-    const uuid& fs::get_uuid_from_resource_path(const std::string& path) {
-        auto it = fs_uuid_map.find(path);
-        if (it == fs_uuid_map.end()) return uuid::null();
-        return it->second;
-    }
+const fs_resource_info& fs::get_resource_info(const uuid& id) {
+    return fs_map.at(id);
+}
 
-    void fs::save_resource_info(const uuid& id, const fs_resource_info& info) {
-        auto fs_it = fs_map.find(id);
-        if (fs_it != fs_map.end()) {
-            fs_uuid_map.erase(fs_it->second.path);
-        }
-        auto id_it = fs_uuid_map.find(info.path);
-        if (id_it != fs_uuid_map.end()) {
-            fs_map.erase(id_it->second);
-        }
+const uuid& fs::get_uuid_from_resource_path(const std::string& path) {
+    auto it = fs_uuid_map.find(path);
+    if (it == fs_uuid_map.end()) return uuid::null();
+    return it->second;
+}
+
+void fs::save_resource_info(const uuid& id, const fs_resource_info& info) {
+    auto fs_it = fs_map.find(id);
+    if (fs_it != fs_map.end()) {
+        fs_uuid_map.erase(fs_it->second.path);
+    }
+    auto id_it = fs_uuid_map.find(info.path);
+    if (id_it != fs_uuid_map.end()) {
+        fs_map.erase(id_it->second);
+    }
+    fs_uuid_map[info.path] = id;
+    fs_map[id] = info;
+}
+
+void fs::save_resource_info(const uuid& id, fs_resource_info&& info) {
+    auto fs_it = fs_map.find(id);
+    if (fs_it != fs_map.end()) {
+        fs_uuid_map.erase(fs_it->second.path);
+    }
+    auto id_it = fs_uuid_map.find(info.path);
+    if (id_it != fs_uuid_map.end()) {
+        fs_map.erase(id_it->second);
+    }
+    fs_uuid_map[info.path] = id;
+    fs_map[id] = std::move(info);
+}
+
+void fs::delete_resource_info(const uuid& id) {
+    fs_uuid_map.erase(fs_map.at(id).path);
+    fs_map.erase(id);
+}
+
+void fs::delete_resource_data(const uuid& id) {
+    std::filesystem::path path = project::project_fs_path / id.str();
+    path.make_preferred();
+    if (std::filesystem::exists(path)) std::filesystem::remove_all(path);
+}
+
+void fs::load_resources() {
+    ELM_INFO("Loading FS map...");
+    ELM_DEBUG("FS map path is {0}", project::project_metadata_fsmap.string());
+    std::ifstream file(project::project_metadata_fsmap);
+    if (file.fail()) {
+        ELM_WARN("Couldn't open FS map");
+        return;
+    }
+    text_deserializer deserialize = create_text_deserializer(file);
+    deserialize(ELM_SERIALIZE_NVP("fs_map", fs_map));
+    fs_uuid_map.reserve(fs_map.size());
+    for (const auto& [id, info] : fs_map) {
         fs_uuid_map[info.path] = id;
-        fs_map[id] = info;
     }
+}
 
-    void fs::save_resource_info(const uuid& id, fs_resource_info&& info) {
-        auto fs_it = fs_map.find(id);
-        if (fs_it != fs_map.end()) {
-            fs_uuid_map.erase(fs_it->second.path);
-        }
-        auto id_it = fs_uuid_map.find(info.path);
-        if (id_it != fs_uuid_map.end()) {
-            fs_map.erase(id_it->second);
-        }
-        fs_uuid_map[info.path] = id;
-        fs_map[id] = std::move(info);
-    }
+void fs::save_resources() {
+    ELM_INFO("Saving FS map...");
+    ELM_DEBUG("FS map path is {0}", project::project_metadata_fsmap.string());
+    std::ofstream file(project::project_metadata_fsmap);
+    text_serializer serialize = create_text_serializer(file);
+    serialize(ELM_SERIALIZE_NVP("fs_map", fs_map));
+}
 
-    void fs::delete_resource_info(const uuid& id) {
-        fs_uuid_map.erase(fs_map.at(id).path);
-        fs_map.erase(id);
-    }
+void fs::save_bin_resources() {
+    ELM_INFO("Saving FS map to binary...");
+    ELM_DEBUG("FS map path is {0}", project::project_fs_fsmap.string());
+    std::ofstream file(project::project_fs_fsmap, std::ios::binary);
+    binary_serializer serialize = create_binary_serializer(file);
+    serialize(ELM_SERIALIZE_NVP("fs_map", fs_map));
+}
 
-    void fs::delete_resource_data(const uuid& id) {
-        std::filesystem::path path = project::project_fs_path / id.str();
-        path.make_preferred();
-        if (std::filesystem::exists(path)) std::filesystem::remove_all(path);
-    }
+uuid fs::get_new_uuid() {
+    uuid id;
+    while (fs_map.contains(id)) id.regenerate();
+    return id;
+}
 
-    void fs::load_resources() {
-        ELM_INFO("Loading FS map...");
-        ELM_DEBUG("FS map path is {0}", project::project_metadata_fsmap.string());
-        std::ifstream file(project::project_metadata_fsmap);
-        if (file.fail()) {
-            ELM_WARN("Couldn't open FS map");
-            return;
-        }
-        text_deserializer deserialize = create_text_deserializer(file);
-        deserialize(ELM_SERIALIZE_NVP("fs_map", fs_map));
-        fs_uuid_map.reserve(fs_map.size());
-        for (const auto& [id, info] : fs_map) {
-            fs_uuid_map[info.path] = id;
-        }
-    }
-
-    void fs::save_resources() {
-        ELM_INFO("Saving FS map...");
-        ELM_DEBUG("FS map path is {0}", project::project_metadata_fsmap.string());
-        std::ofstream file(project::project_metadata_fsmap);
-        text_serializer serialize = create_text_serializer(file);
-        serialize(ELM_SERIALIZE_NVP("fs_map", fs_map));
-    }
-
-    void fs::save_bin_resources() {
-        ELM_INFO("Saving FS map to binary...");
-        ELM_DEBUG("FS map path is {0}", project::project_fs_fsmap.string());
-        std::ofstream file(project::project_fs_fsmap, std::ios::binary);
-        binary_serializer serialize = create_binary_serializer(file);
-        serialize(ELM_SERIALIZE_NVP("fs_map", fs_map));
-    }
-
-    uuid fs::get_new_uuid() {
-        uuid id;
-        while (fs_map.contains(id)) id.regenerate();
-        return id;
-    }
-
-    const std::unordered_map<element::uuid, element::fs_resource_info>& fs::get_map() {
-        return fs_map;
-    }
-
-} // namespace element
+const std::unordered_map<element::uuid, element::fs_resource_info>& fs::get_map() {
+    return fs_map;
+}
