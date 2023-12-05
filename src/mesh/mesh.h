f779d878ee63909bc3abab1dc7e2a5efa932d374
Lander Gallastegi <landergallastegi@gmail.com>
2023-12-05 16:06:28 +0100

    Mesh asset

---
+++ src/mesh/mesh.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <glm/glm.hpp>
+#include <cinttypes>
+#include <vector>
+
+namespace element {
+    namespace mesh {
+
+        struct mesh {
+            std::uint32_t version = 0;
+            std::vector<glm::vec3> positions;
+            std::vector<glm::vec3> normals;
+            std::vector<glm::vec3> tangents;
+            std::vector<glm::vec2> tex_coords;
+        };
+
+    } //namespace mesh
+} //namespace element
