25a8c7cd3d8a14f676a9763332e3a0226146bc6e
Lander Gallastegi <landergallastegi@gmail.com>
2023-12-27 21:11:00 +0100

    Fix constness

--- src/scenegraph/editor_camera_node_storage.h
+++ src/scenegraph/editor_camera_node_storage.h
@@ -10,6 +10,7 @@ namespace element {
         class editor_camera_node_storage : public node_storage_base {
             private:
                 camera_node editor_cam;
+                camera_node_ref editor_cam_ref;
             public:
                 editor_camera_node_storage();
                 ~editor_camera_node_storage() final override;
@@ -19,11 +20,13 @@ namespace element {
                 void emplace_node(const uuid& id, std::string&& name, node_ref& parent) final override;
                 void emplace_root(const uuid& id) final override;
                 scenegraph::node* get_node_ptr(const uuid& id) final override;
-                std::type_index get_type_index() final override;
+                std::type_index get_type_index() const final override;
                 void init_scene(scenegraph::scene* owner_scene) final override;
 
                 camera_node_ref& get_editor_camera();
         };
+
+        camera_node_ref& get_editor_camera();
 
     } // namespace scenegraph 
 } // namespace element 
