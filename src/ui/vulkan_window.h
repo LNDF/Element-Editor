ed13da9e3ca8bfb1c9786220da0eb023d077a2d7
Lander Gallastegi <landergallastegi@gmail.com>
2023-12-29 13:47:05 +0100

    Make resizeEvent protected

--- src/ui/vulkan_window.h
+++ src/ui/vulkan_window.h
@@ -12,11 +12,11 @@ namespace element {
             bool swapchain_created = false;
             vk::SurfaceKHR surface;
             vulkan::swapchain_info swapchain;
+        protected:
+            void resizeEvent(QResizeEvent *ev) override;
         public:
             qt_vulkan_window(QWindow* parent = nullptr);
-            virtual ~qt_vulkan_window();
-
-            void resizeEvent(QResizeEvent *ev) override;
+            virtual ~qt_vulkan_window();disc
     };
 
 } // namespace element
