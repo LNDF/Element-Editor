03855ef7518a6e52e5a3e99c617827ecb934d68e
Lander Gallastegi <landergallastegi@gmail.com>
2023-12-30 19:58:44 +0100

    Use pipeline asset loader on importer

--- src/asset/importers/pipeline.cpp
+++ src/asset/importers/pipeline.cpp
@@ -1,11 +1,11 @@
 #include <asset/importer.h>
 #include <asset/dependencies.h>
+#include <asset/loaders/pipeline.h>
 #include <core/fs_editor.h>
-#include <editor/project.h>
 #include <render/pipeline.h>
 #include <render/pipeline_process.h>
 #include <serialization/defs.h>
-#include <serialization/serializers_editor.h>
+#include <serialization/serializers.h>
 #include <serialization/render/pipeline.h>
 #include <utils/uuid.h>
 #include <utils/packed_set.h>
@@ -15,20 +15,17 @@ using namespace element;
 
 static void pipeline_importer(const uuid& id) {
     fs_resource_info info = fs::get_resource_info(id);
-    std::ifstream input(project::project_assets_path / info.path);
-    render::pipeline_data data;
-    text_deserializer deserialize = create_text_deserializer(input);
-    deserialize(ELM_SERIALIZE_NVP("pipeline", data));
-    if (render::pipeline_create_layout_data(data)) {
+    std::optional<render::pipeline_data> data = asset_loaders::pipeline_text_load(info.path);
+    if (render::pipeline_create_layout_data(data.value())) {
         auto output = fs::get_resource_ostream(id);
         binary_serializer serialize = create_binary_serializer(*output);
-        serialize(data);
+        serialize(data.value());
     } else {
         fs::delete_resource_data(id);
     }
     packed_set<uuid> dependencies;
-    dependencies.insert(data.vert_id);
-    dependencies.insert(data.frag_id);
+    dependencies.insert(data->vert_id);
+    dependencies.insert(data->frag_id);
     asset_importer::set_id_dependencies(id, std::move(dependencies));
 }
 
