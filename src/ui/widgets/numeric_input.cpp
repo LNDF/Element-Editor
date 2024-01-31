6aad94682dd508eb034f013db00642fb9e4ef7b5
Lander Gallastegi <landergallastegi@gmail.com>
2024-01-31 13:28:40 +0100

    HACK: no minimum size in spinners

--- src/ui/widgets/numeric_input.cpp
+++ src/ui/widgets/numeric_input.cpp
@@ -5,7 +5,7 @@
 using namespace element::ui;
 
 integer_input::integer_input(QWidget* parent) : QSpinBox(parent) {
-    QSizePolicy size_policy(QSizePolicy::MinimumExpanding, QSizePolicy::Preferred);
+    QSizePolicy size_policy(QSizePolicy::Expanding, QSizePolicy::Preferred);
     size_policy.setHorizontalStretch(0);
     size_policy.setVerticalStretch(0);
     size_policy.setHeightForWidth(sizePolicy().hasHeightForWidth());
@@ -16,14 +16,28 @@ integer_input::integer_input(QWidget* parent) : QSpinBox(parent) {
     setStepType(QAbstractSpinBox::AdaptiveDecimalStepType);
 }
 
-double_input::double_input(QWidget* parent) : QDoubleSpinBox(parent) {
-    QSizePolicy size_policy(QSizePolicy::MinimumExpanding, QSizePolicy::Preferred);
+QString integer_input::textFromValue(int value) const { //This is a hack to allow minimum size to be 0
+    if (value == std::numeric_limits<int>::min() || value == std::numeric_limits<int>::max()) {
+        return "";
+    }
+    return QSpinBox::textFromValue(value);
+}
+
+real_input::real_input(QWidget* parent) : QDoubleSpinBox(parent) {
+    QSizePolicy size_policy(QSizePolicy::Expanding, QSizePolicy::Preferred);
     size_policy.setHorizontalStretch(0);
     size_policy.setVerticalStretch(0);
     size_policy.setHeightForWidth(sizePolicy().hasHeightForWidth());
     setSizePolicy(size_policy);
     setMinimumSize(QSize(0, 0));
-    setMinimum(-std::numeric_limits<double>::max());
-    setMaximum(std::numeric_limits<double>::max());
+    setMinimum(std::numeric_limits<int>::min());
+    setMaximum(std::numeric_limits<int>::max());
     setStepType(QAbstractSpinBox::AdaptiveDecimalStepType);
+}
+
+QString real_input::textFromValue(double value) const { //This is a hack to allow minimum size to be 0
+    if (value == std::numeric_limits<int>::min() || value == std::numeric_limits<int>::max()) {
+        return "";
+    }
+    return QDoubleSpinBox::textFromValue(value);
 }
