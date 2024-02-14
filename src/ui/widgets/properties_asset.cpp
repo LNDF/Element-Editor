42ab907fab66f02e5ac58bce6656588f0ec45b0f
Lander Gallastegi <landergallastegi@gmail.com>
2024-02-14 23:25:35 +0100

    Redesign asset properties

--- src/ui/widgets/properties_asset.cpp
+++ src/ui/widgets/properties_asset.cpp
@@ -2,6 +2,18 @@
 
 using namespace element::ui;
 
+std::unordered_map<std::string, element::ui::properties_asset_container_factory>& element::__detail::__ui_get_asset_properties_container_map() {
+    static std::unordered_map<std::string, properties_asset_container_factory> map;
+    return map;
+}
+
+properties_asset_container_factory element::ui::get_asset_properties_container_factory(const std::string& type) {
+    auto& map = element::__detail::__ui_get_asset_properties_container_map();
+    auto it = map.find(type);
+    if (it == map.end()) return nullptr;
+    return it->second;
+}
+
 properties_asset::properties_asset(const uuid& id, QWidget* parent) : properties_container(parent) {
     
 }
