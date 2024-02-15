8d5a083e4ebd180657ac7e6af3028205a21fd1b9
Lander Gallastegi <landergallastegi@gmail.com>
2024-02-15 17:36:04 +0100

    Save reload

--- src/ui/widgets/properties_asset.cpp
+++ src/ui/widgets/properties_asset.cpp
@@ -1,7 +1,22 @@
 #include "properties_asset.h"
 
+#include <QCoreApplication>
+
 using namespace element::ui;
 
 properties_asset::properties_asset(const uuid& id, QWidget* parent) : properties_container(parent) {
-    
+    save_reload = new QWidget(this);
+    save_reload_layout = new QHBoxLayout(save_reload);
+    save_reload_space = new QSpacerItem(40, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
+    save_reload_save = new QPushButton(save_reload);
+    save_reload_reload = new QPushButton(save_reload);
+    save_reload->setObjectName("save_reload");
+    save_reload_layout->setObjectName("save_reload_layout");
+    save_reload_save->setObjectName("save_reload_save");
+    save_reload_reload->setObjectName("save_reload_reload");
+    save_reload_save->setText(QCoreApplication::translate("element-editor", "Save"));
+    save_reload_reload->setText(QCoreApplication::translate("element-editor", "Reload"));
+    save_reload_layout->addItem(save_reload_space);
+    save_reload_layout->addWidget(save_reload_save);
+    save_reload_layout->addWidget(save_reload_reload);
 }
--- src/ui/widgets/properties_asset.h
+++ src/ui/widgets/properties_asset.h
@@ -2,12 +2,22 @@
 
 #include <ui/widgets/properties_container.h>
 #include <utils/uuid.h>
+#include <QWidget>
+#include <QSpacerItem>
+#include <QHBoxLayout>
+#include <QPushButton>
 
 namespace element {
     namespace ui {
         
         class properties_asset : public properties_container {
             Q_OBJECT
+            private:
+                QWidget* save_reload;
+                QHBoxLayout* save_reload_layout;
+                QSpacerItem* save_reload_space;
+                QPushButton* save_reload_save;
+                QPushButton* save_reload_reload;
             public:
                 properties_asset(const uuid& id, QWidget* parent = nullptr);
         };
