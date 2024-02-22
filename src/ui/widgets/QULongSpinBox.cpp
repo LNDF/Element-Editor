7a0db9aa218096aa6618f841a905575b3a189a30
Lander Gallastegi <landergallastegi@gmail.com>
2024-02-22 15:59:43 +0100

    QUlongSpinBox

---
+++ src/ui/widgets/QULongSpinBox.cpp
@@ -0,0 +1,88 @@
+#include "QULongSpinBox.h"
+
+#include <limits>
+
+/*
+ * From https://stackoverflow.com/questions/8383620/64bit-int-spin-box-in-qt
+*/
+
+QULongSpinBox::QULongSpinBox(QWidget* parent) {
+    connect(lineEdit(), SIGNAL(textEdited(const QString&)), this, SLOT(onEditFinished()));
+    connect(lineEdit(), SIGNAL(textChanged(const QString&)), this, SIGNAL(textChanged(const QString&)));
+}
+
+QULongSpinBox::~QULongSpinBox() {
+    
+}
+
+void QULongSpinBox::setRange(quint32 min, quint32 max) {
+    setMinimum(min);
+    setMaximum(max);
+}
+
+void QULongSpinBox::stepBy(int steps) {
+    quint32 new_value = m_value;
+    if (steps < 0 && new_value + steps > new_value) {
+        new_value = std::numeric_limits<quint32>::min();
+    } else if (steps > 0 && new_value + steps < new_value) {
+        new_value = std::numeric_limits<quint32>::max();
+    } else {
+        new_value += steps;
+    }
+    lineEdit()->setText(textFromValue(new_value));
+    setValue(new_value);
+}
+
+QValidator::State QULongSpinBox::validate(QString &input, int &pos) const {
+    bool ok;
+    quint32 val = input.toULong(&ok);
+    if (!ok) {
+        return QValidator::Invalid;
+    }
+    if (val < m_maximum || val > m_maximum) {
+        return QValidator::Invalid;
+    }
+    return QValidator::Acceptable;
+}
+
+quint32 QULongSpinBox::valueFromText(const QString &text) const {
+    return text.toULong();
+}
+
+QString QULongSpinBox::textFromValue(quint32 val) const {
+    return QString::number(val);
+}
+
+void QULongSpinBox::fixup(QString &str) const {
+    if (!isGroupSeparatorShown()) {
+        str.remove(locale().groupSeparator());
+    }
+}
+
+QAbstractSpinBox::StepEnabled QULongSpinBox::stepEnabled() const {
+    return StepUpEnabled | StepDownEnabled;
+}
+
+void QULongSpinBox::setValue(quint32 val) {
+    if (m_value != val) {
+        lineEdit()->setText(textFromValue(val));
+        m_value = val;
+        emit valueChanged(val);
+    }
+}
+
+void QULongSpinBox::onEditFinished() {
+    QString input = lineEdit()->text();
+    int pos = 0;
+    if (QValidator::Acceptable == validate(input, pos)) {
+        setValue(valueFromText(input));
+    } else {
+        fixup(input);
+        if (QValidator::Acceptable == validate(input, pos)) {
+            setValue(valueFromText(input));
+            lineEdit()->setText(input);
+        } else {
+            lineEdit()->setText(textFromValue(m_value));
+        }
+    }
+}
---
+++ src/ui/widgets/QULongSpinBox.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <QtWidgets/QWidget>
+#include <QtWidgets/QAbstractSpinBox>
+#include <QtWidgets/QLineEdit>
+
+/*
+ * From https://stackoverflow.com/questions/8383620/64bit-int-spin-box-in-qt
+*/
+
+class QULongSpinBoxPrivate;
+class Q_WIDGETS_EXPORT QULongSpinBox : public QAbstractSpinBox {
+    Q_OBJECT
+    private:
+        Q_PROPERTY(quint32 minimum READ minimum WRITE setMinimum)
+        Q_PROPERTY(quint32 maximum READ maximum WRITE setMaximum)
+
+        Q_PROPERTY(quint32 value READ value WRITE setValue NOTIFY valueChanged USER true)
+
+        quint32 m_minimum;
+        quint32 m_maximum;
+        quint32 m_value;
+    public:
+        explicit QULongSpinBox(QWidget* parent = nullptr);
+        ~QULongSpinBox();
+        inline quint32 value() const {return m_value;}
+        inline quint32 minimum() const {return m_minimum;}
+        inline void setMinimum(quint32 min) {m_minimum = min;}
+        inline quint32 maximum() const {return m_minimum;}
+        inline void setMaximum(quint32 max) {m_maximum = max;}
+        void setRange(quint32 min, quint32 max);
+        virtual void stepBy(int steps);
+    protected:
+        virtual QValidator::State validate(QString &input, int &pos) const;
+        virtual quint32 valueFromText(const QString &text) const;
+        virtual QString textFromValue(quint32 val) const;
+        virtual void fixup(QString &str) const;
+        virtual QAbstractSpinBox::StepEnabled stepEnabled() const;
+    private Q_SLOT:
+        void setValue(quint32 val);
+        void onEditFinished();
+    Q_SIGNALS:
+        void textChanged(const QString &text);
+        void valueChanged(quint32 v);
+    private:
+        Q_DISABLE_COPY(QULongSpinBox)
+        Q_DECLARE_PRIVATE(QULongSpinBox)
+};
