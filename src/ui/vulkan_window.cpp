8c18e350a52a689adc4165e2e8a232c9140931ab
Lander Gallastegi <landergallastegi@gmail.com>
2024-02-27 12:56:20 +0100

    Implement editor camera movement

--- src/ui/vulkan_window.cpp
+++ src/ui/vulkan_window.cpp
@@ -4,12 +4,14 @@
 #include <core/log.h>
 #include <render/vulkan_swapchain.h>
 #include <render/render.h>
+#include <scenegraph/editor_camera_node_storage.h>
+#include <glm/glm.hpp>
 
 #include <QResizeEvent>
 
 using namespace element;
 
-qt_vulkan_window::qt_vulkan_window(QWindow* parent) : QWindow(parent) {
+qt_vulkan_window::qt_vulkan_window(QWindow* parent) : QWindow(parent), drag_status(qt_vulkan_window_drag::none) {
     create();
     surface = vulkan::create_surface_from_qt(this);
     if (!vulkan::device_initialized) {
@@ -40,6 +42,55 @@ void qt_vulkan_window::resizeEvent(QResizeEvent* ev) {
         scenegraph::open_scene("test.scene");
     }
     render();
+}
+
+void qt_vulkan_window::mouseMoveEvent(QMouseEvent* ev) {
+    if (drag_status != qt_vulkan_window_drag::none) {
+        QPoint pos = QCursor::pos();
+        QPoint center = mapToGlobal(QPoint(width() / 2, height() / 2));
+        if (pos == center) return;
+        QCursor::setPos(center);
+        scenegraph::transform& trans = scenegraph::get_editor_camera()->get_transform();
+        float dx = (float) (pos.x() - center.x()) / -width();
+        float dy = (float) (pos.y() - center.y()) / height();
+        if (drag_status == qt_vulkan_window_drag::translate) {
+            glm::vec3 pos = trans.get_position();
+            pos += dx * trans.get_right();
+            pos += dy * trans.get_up();
+            trans.set_position(pos);
+        } else if (drag_status == qt_vulkan_window_drag::rotate) {
+            trans.rotate_axis(trans.get_right(), dy);
+            glm::vec3 up(0, 1, 0);
+            if (trans.get_up().y > 0) up *= -1;
+            trans.rotate_axis(up, dx);
+        }
+        render();
+    }
+}
+
+void qt_vulkan_window::mousePressEvent(QMouseEvent* ev) {
+    if (drag_status == qt_vulkan_window_drag::none) {
+        if (ev->button() == Qt::RightButton) {
+            drag_status = qt_vulkan_window_drag::rotate;
+            pre_drag_pos = QCursor::pos();
+            setCursor(Qt::BlankCursor);
+            QCursor::setPos(mapToGlobal(QPoint(width() / 2, height() / 2)));
+        } else if (ev->button() == Qt::MiddleButton) {
+            drag_status = qt_vulkan_window_drag::translate;
+            pre_drag_pos = QCursor::pos();
+            setCursor(Qt::BlankCursor);
+            QCursor::setPos(mapToGlobal(QPoint(width() / 2, height() / 2)));
+        }
+    }
+}
+
+void qt_vulkan_window::mouseReleaseEvent(QMouseEvent* ev) {
+    if ((drag_status == qt_vulkan_window_drag::translate && ev->button() == Qt::MiddleButton) ||
+        (drag_status == qt_vulkan_window_drag::rotate && ev->button() == Qt::RightButton)) {
+        drag_status = qt_vulkan_window_drag::none;
+        QCursor::setPos(pre_drag_pos);
+        setCursor(Qt::ArrowCursor);
+    }
 }
 
 void qt_vulkan_window::render() {
--- src/ui/vulkan_window.h
+++ src/ui/vulkan_window.h
@@ -6,14 +6,25 @@
 
 namespace element {
     
+    enum qt_vulkan_window_drag {
+        none,
+        rotate,
+        translate
+    };
+
     class qt_vulkan_window : public QWindow {
         Q_OBJECT
         private:
             bool swapchain_created = false;
             vk::SurfaceKHR surface;
             vulkan::swapchain_info swapchain;
+            qt_vulkan_window_drag drag_status;
+            QPoint pre_drag_pos;
         protected:
             void resizeEvent(QResizeEvent *ev) override;
+            void mouseMoveEvent(QMouseEvent *ev) override;
+            void mousePressEvent(QMouseEvent *ev) override;
+            void mouseReleaseEvent(QMouseEvent *ev) override;
         public:
             qt_vulkan_window(QWindow* parent = nullptr);
             virtual ~qt_vulkan_window();
