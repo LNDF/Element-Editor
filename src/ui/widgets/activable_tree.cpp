2bbb53d1d6529919395b2702c6fd8a4c9e2dddf1
Lander Gallastegi <landergallastegi@gmail.com>
2024-03-07 22:25:23 +0100

    Activable tree part 3

--- src/ui/widgets/activable_tree.cpp
+++ src/ui/widgets/activable_tree.cpp
@@ -3,18 +3,35 @@
 using namespace element::ui;
 
 activable_tree::activable_tree(QWidget* parent) : QTreeView(parent) {
-    
+
 }
 
 #include <core/log.h>
-void activable_tree::mousePressEvent(QMouseEvent *event) {
+
+void activable_tree::mousePressEvent(QMouseEvent* event) {
+    do_click_select = true;
+    select_index = indexAt(event->pos());
     QTreeView::mousePressEvent(event);
 }
 
-void activable_tree::mouseReleaseEvent(QMouseEvent *event) {
+void activable_tree::mouseReleaseEvent(QMouseEvent* event) {
+    if (do_click_select && select_index.isValid()) {
+        emit activate(select_index);
+    }
+    do_click_select = false;
     QTreeView::mouseReleaseEvent(event);
 }
 
-void activable_tree::dragMoveEvent(QDragMoveEvent *event) {
+void activable_tree::dragMoveEvent(QDragMoveEvent* event) {
+    do_click_select = false;
     QTreeView::dragMoveEvent(event);
+}
+
+void activable_tree::setModel(QAbstractItemModel* model) {
+    QTreeView::setModel(model);
+    connect(selectionModel(), SIGNAL(currentChanged(const QModelIndex&, const QModelIndex&)), this, SLOT(on_selection_changed(const QModelIndex&)), Qt::ConnectionType::DirectConnection);
+}
+
+void activable_tree::on_selection_changed(const QModelIndex& index) {
+    if (!do_click_select) emit activate(index);
 }
--- src/ui/widgets/activable_tree.h
+++ src/ui/widgets/activable_tree.h
@@ -10,14 +10,21 @@ namespace element {
         
         class activable_tree : public QTreeView {
             Q_OBJECT
+            private:
+                QModelIndex select_index;
+                bool do_click_select = false;
             protected:
                 virtual void mousePressEvent(QMouseEvent *event);
                 virtual void mouseReleaseEvent(QMouseEvent *event);
                 virtual void dragMoveEvent(QDragMoveEvent *event);
             public:
                 activable_tree(QWidget* parent);
+
+                virtual void setModel(QAbstractItemModel* model);
+            private slots:
+                void on_selection_changed(const QModelIndex& index);
             signals:
-                
+                void activate(const QModelIndex& index);
         };
 
     } // namespace ui
