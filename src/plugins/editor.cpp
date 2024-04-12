95b38ddbf6fb91f7d0a5a8e8683a4a40654b6956
Lander Gallastegi <landergallastegi@gmail.com>
2024-04-12 19:44:15 +0200

    Editor platform list

---
+++ src/plugins/editor.cpp
@@ -0,0 +1,8 @@
+#include "editor.h"
+
+using namespace element;
+
+std::array<const char*, 2> plugins::platforms = {
+    "windows",
+    "linux"
+};
---
+++ src/plugins/editor.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <array>
+
+namespace element {
+    namespace plugins {
+        
+        extern std::array<const char*, 2> platforms;
+
+    } // namespace plugins    
+} // namespace element
