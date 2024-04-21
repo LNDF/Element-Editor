a96d4f0ec74ca137d26f3e1808addfadf1e9196c
Lander Gallastegi <landergallastegi@gmail.com>
2024-04-21 02:51:43 +0200

    Fix all plugin loading

--- src/plugins/editor.cpp
+++ src/plugins/editor.cpp
@@ -16,6 +16,6 @@ void plugins::load_plugins() {
     ELM_INFO("Loading plugins...");
     unload_all_plugins();
     for (const auto& platform_plugin : project::plugins[ELM_PLATFORM_STRING]) {
-        load_plugin(project::project_bin_path / platform_plugin);
+        load_plugin(project::project_bin_path / ELM_PLATFORM_STRING / platform_plugin);
     }
 }
