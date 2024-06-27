dabd23e1e7de969eb4727f34cd08d7ce5a57f1e2
Lander Gallastegi <landergallastegi@gmail.com>
2024-06-27 18:39:51 +0200

    Make pipelines non transparent by default

--- src/asset/creators/pipeline.cpp
+++ src/asset/creators/pipeline.cpp
@@ -7,6 +7,7 @@ static void asset_create_pipeline(const std::string& path) {
     element::render::pipeline_data pipeline;
     pipeline.vert_id = element::uuid::null();
     pipeline.frag_id = element::uuid::null();
+    pipeline.transparent = false;
     element::asset_loaders::pipeline_text_save(path, pipeline);
 }
 
